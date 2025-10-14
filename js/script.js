a4703e9fed11418fa3f8caf80300d17566d25d24
subharamv <subharam.v@clovetech.com>
2025-10-14 13:28:01 +0000

    Changed product v1.0

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Dynamically determine correct path prefix based on current location
-const pathPrefix = window.location.pathname.includes('/Solutions/') ? '../' : './';
+const pathPrefix = (window.location.pathname.includes('/Solutions/') || window.location.pathname.includes('/Products/')) ? '../' : './';
 Promise.all([
         fetch(pathPrefix + "header.html").then(res => res.text()),
         fetch(pathPrefix + "footer.html").then(res => res.text()),
